1b9cb5cf82abf0852417287804f42fa4e0243301
Kamioda <kamiodamasato@outlook.com>
2024-06-18 23:33:46 +0900

    引数が逆になっていたのを修正

--- modules/Number.cpp
+++ modules/Number.cpp
@@ -8,112 +8,112 @@ IUTEST_TYPED_TEST_CASE(NumberTest, ::iutest::Types<short, int, long, long long, 
 IUTEST_TYPED_TEST(NumberTest, Constructor) {
     using Number = kamioda::Number<TypeParam>;
     Number num{};
-    IUTEST_ASSERT_EQ(num, 0);
+    IUTEST_ASSERT_EQ(0, num);
     Number num2(10);
-    IUTEST_ASSERT_EQ(num2, 10);
+    IUTEST_ASSERT_EQ(10, num2);
     Number num3(10, 5, 15);
-    IUTEST_ASSERT_EQ(num3, 10);
+    IUTEST_ASSERT_EQ(10, num3);
     Number num4(num3);
-    IUTEST_ASSERT_EQ(num4, 10);
+    IUTEST_ASSERT_EQ(10, num4);
     Number num5(std::move(num4));
-    IUTEST_ASSERT_EQ(num5, 10);
+    IUTEST_ASSERT_EQ(10, num5);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Assignment) {
     using Number = kamioda::Number<TypeParam>;
     Number num;
     num = 10;
-    IUTEST_ASSERT_EQ(num, 10);
+    IUTEST_ASSERT_EQ(10, num);
     Number num2;
     num2 = num;
-    IUTEST_ASSERT_EQ(num2, 10);
+    IUTEST_ASSERT_EQ(10, num2);
     Number num3;
     num3 = std::move(num2);
-    IUTEST_ASSERT_EQ(num3, 10);
+    IUTEST_ASSERT_EQ(10, num3);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Addition) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
     num += Number(5);
-    IUTEST_ASSERT_EQ(num, 15);
+    IUTEST_ASSERT_EQ(15, num);
     num += 5;
-    IUTEST_ASSERT_EQ(num, 20);
+    IUTEST_ASSERT_EQ(20, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Increment) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    IUTEST_ASSERT_EQ(num++, 10);
-    IUTEST_ASSERT_EQ(num, 11);
-    IUTEST_ASSERT_EQ(++num, 12);
+    IUTEST_ASSERT_EQ(10, num++);
+    IUTEST_ASSERT_EQ(11, num);
+    IUTEST_ASSERT_EQ(12, ++num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Subtraction) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
     num -= Number(5);
-    IUTEST_ASSERT_EQ(num, 5);
+    IUTEST_ASSERT_EQ(5, num);
     num -= 5;
-    IUTEST_ASSERT_EQ(num, 0);
+    IUTEST_ASSERT_EQ(0, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Decrement) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    IUTEST_ASSERT_EQ(num--, 10);
-    IUTEST_ASSERT_EQ(num, 9);
-    IUTEST_ASSERT_EQ(--num, 8);
+    IUTEST_ASSERT_EQ(10, num--);
+    IUTEST_ASSERT_EQ(9, num);
+    IUTEST_ASSERT_EQ(8, --num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Multiplication) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
     num *= Number(5);
-    IUTEST_ASSERT_EQ(num, 50);
+    IUTEST_ASSERT_EQ(50, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Division) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
     num /= Number(5);
-    IUTEST_ASSERT_EQ(num, 2);
+    IUTEST_ASSERT_EQ(2, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Modulus) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
     num %= Number(5);
-    IUTEST_ASSERT_EQ(num, 0);
+    IUTEST_ASSERT_EQ(0, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Clamp) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10, 5, 15);
-    IUTEST_ASSERT_EQ(num, 10);
+    IUTEST_ASSERT_EQ(10, num);
     num = 20;
-    IUTEST_ASSERT_EQ(num, 15);
+    IUTEST_ASSERT_EQ(15, num);
     num = 0;
-    IUTEST_ASSERT_EQ(num, 5);
+    IUTEST_ASSERT_EQ(5, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Comparison) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    IUTEST_ASSERT_EQ(num, 10);
-    IUTEST_ASSERT_NE(num, 5);
-    IUTEST_ASSERT_LT(num, 15);
-    IUTEST_ASSERT_LE(num, 15);
-    IUTEST_ASSERT_GT(num, 5);
-    IUTEST_ASSERT_GE(num, 5);
+    IUTEST_ASSERT_EQ(10, num);
+    IUTEST_ASSERT_NE(5, num);
+    IUTEST_ASSERT_GT(15, num);
+    IUTEST_ASSERT_GE(15, num);
+    IUTEST_ASSERT_LT(5, num);
+    IUTEST_ASSERT_LE(5, num);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Stream) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    std::ostringstream oss;
+    std::ostringstream oss{};
     oss << num;
-    IUTEST_ASSERT_EQ(oss.str(), "10");
+    IUTEST_ASSERT_EQ("10", oss.str());
 }
 
 IUTEST_TYPED_TEST(NumberTest, Exception) {
@@ -125,25 +125,25 @@ IUTEST_TYPED_TEST(NumberTest, Exception) {
 IUTEST_TYPED_TEST(NumberTest, Cast) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    IUTEST_ASSERT_EQ(num.get<int>(), 10);
-    IUTEST_ASSERT_EQ(num.get<long>(), 10);
-    IUTEST_ASSERT_EQ(num.get<long long>(), 10);
+    IUTEST_ASSERT_EQ(10, num.get<int>());
+    IUTEST_ASSERT_EQ(10, num.get<long>());
+    IUTEST_ASSERT_EQ(10, num.get<long long>());
 }
 
 IUTEST_TYPED_TEST(NumberTest, Arithmetic) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    IUTEST_ASSERT_EQ(~num, static_cast<TypeParam>(-11));
-    IUTEST_ASSERT_EQ(num + Number(5), 15);
-    IUTEST_ASSERT_EQ(num - Number(5), 5);
-    IUTEST_ASSERT_EQ(num * Number(5), 50);
-    IUTEST_ASSERT_EQ(num / Number(5), 2);
-    IUTEST_ASSERT_EQ(num % Number(5), 0);
-    IUTEST_ASSERT_EQ(num & Number(5), 0);
-    IUTEST_ASSERT_EQ(num | Number(5), 15);
-    IUTEST_ASSERT_EQ(num ^ Number(5), 15);
-    IUTEST_ASSERT_EQ(num << 1, 20);
-    IUTEST_ASSERT_EQ(num >> 1, 5);
+    IUTEST_ASSERT_EQ(static_cast<TypeParam>(-11), ~num);
+    IUTEST_ASSERT_EQ(15, num + Number(5));
+    IUTEST_ASSERT_EQ(5, num - Number(5));
+    IUTEST_ASSERT_EQ(50, num * Number(5));
+    IUTEST_ASSERT_EQ(2, num / Number(5));
+    IUTEST_ASSERT_EQ(0, num % Number(5));
+    IUTEST_ASSERT_EQ(0, num & Number(5));
+    IUTEST_ASSERT_EQ(15, num | Number(5));
+    IUTEST_ASSERT_EQ(15, num ^ Number(5));
+    IUTEST_ASSERT_EQ(20, num << 1);
+    IUTEST_ASSERT_EQ(5, num >> 1);
 }
 
 IUTEST_TYPED_TEST(NumberTest, Logical) {
@@ -158,10 +158,10 @@ IUTEST_TYPED_TEST(NumberTest, Logical) {
 IUTEST_TYPED_TEST(NumberTest, Bitwise) {
     using Number = kamioda::Number<TypeParam>;
     Number num(10);
-    IUTEST_ASSERT_EQ(num & Number(5), 0);
-    IUTEST_ASSERT_EQ(num | Number(5), 15);
-    IUTEST_ASSERT_EQ(num ^ Number(5), 15);
-    IUTEST_ASSERT_EQ(~num, static_cast<TypeParam>(-11));
-    IUTEST_ASSERT_EQ(num << 1, 20);
-    IUTEST_ASSERT_EQ(num >> 1, 5);
+    IUTEST_ASSERT_EQ(0, num & Number(5));
+    IUTEST_ASSERT_EQ(15, num | Number(5));
+    IUTEST_ASSERT_EQ(15, num ^ Number(5));
+    IUTEST_ASSERT_EQ(static_cast<TypeParam>(-11), ~num);
+    IUTEST_ASSERT_EQ(20, num << 1);
+    IUTEST_ASSERT_EQ(5, num >> 1);
 }
