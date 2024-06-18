0edb4027a6faa5eb7c4d136f17eda77aa31ec8b8
Kamioda <kamiodamasato@outlook.com>
2024-06-18 23:35:04 +0900

    コンストラクターの引数に与える値が逆になっているのを修正

--- modules/CharacterParameter.cpp
+++ modules/CharacterParameter.cpp
@@ -7,16 +7,16 @@ IUTEST_TYPED_TEST_CASE(CharacterParameterTest, ::iutest::Types<short, int, long,
 
 IUTEST_TYPED_TEST(CharacterParameterTest, Constructor) {
 	using CharacterParameter = kamioda::CharacterParameter<TypeParam>;
-	CharacterParameter num(1, 0, 200);
-	IUTEST_ASSERT_EQ(num, 1);
-	IUTEST_ASSERT_EQ(num.getRatio(), 1);
-	CharacterParameter num2(199, 0, 200);
-	IUTEST_ASSERT_EQ(num2, 199);
-	IUTEST_ASSERT_EQ(num2.getRatio(), 99);
-	CharacterParameter num3(0, 0, 200);
-	IUTEST_ASSERT_EQ(num3, 0);
-	IUTEST_ASSERT_EQ(num3.getRatio(), 0);
-	CharacterParameter num4(200, 0, 200);
-	IUTEST_ASSERT_EQ(num4, 200);
-	IUTEST_ASSERT_EQ(num4.getRatio(), 100);
+	CharacterParameter num(1, 200, 0);
+	IUTEST_ASSERT_EQ(1, num);
+	IUTEST_ASSERT_EQ(1, num.getRatio());
+	CharacterParameter num2(199, 200, 0);
+	IUTEST_ASSERT_EQ(199, num2);
+	IUTEST_ASSERT_EQ(99, num2.getRatio());
+	CharacterParameter num3(0, 200, 0);
+	IUTEST_ASSERT_EQ(0, num3);
+	IUTEST_ASSERT_EQ(0, num3.getRatio());
+	CharacterParameter num4(200, 200, 0);
+	IUTEST_ASSERT_EQ(200, num4);
+	IUTEST_ASSERT_EQ(100, num4.getRatio());
 }
